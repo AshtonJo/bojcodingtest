7073a9e4b44ee8060fd8519539dbba891ef3c577
AshtonJo <hee576@nate.com>
2025-03-31 18:58:35 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -0,0 +1,9 @@
+leap_year = int(input())
+
+if (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0):
+  print(1)
+else:
+  print(0)
+
+# result =  (1 if (leap_year % 4 == 0 and leap_year % 100 != 0) or (leap_year % 400 == 0) else 0)
+# print(result)
